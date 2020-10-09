340948514d1e2f84653344fdc7557437ac82fbaf
David Lynch <davidjlynch1017@gmail.com>
2020-10-09 09:39:14 -0500

    finished javascript and styles for header except for the menu icon

---
+++ app/assets/scripts/modules/MobileMenu.js
@@ -0,0 +1,20 @@
+class MobileMenu {
+    constructor() {
+        this.menuIcon = document.querySelector('.header__menu-icon');
+        this.menuContent = document.querySelector(".header__menu-content");
+        this.siteHeader = document.querySelector('.header');
+        this.events();
+        
+    }
+
+    events() {
+        this.menuIcon.addEventListener('click', () => this.toggleTheMenu())
+    }
+
+    toggleTheMenu() {
+        this.menuContent.classList.toggle("header__menu-content--is-visible");
+        this.siteHeader.classList.toggle('header--is-expanded');
+    }
+}
+
+export default MobileMenu;
