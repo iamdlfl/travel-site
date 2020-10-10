d8c99a11c7434d7c90fef0df1dcba653d0b2e973
David Lynch <davidjlynch1017@gmail.com>
2020-10-09 22:44:15 -0500

    started reveal on scroll functionality

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,12 @@
 import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu.js'
+import RevealOnScroll from'./modules/RevealOnScroll.js'
+
+let menu = new MobileMenu();
+let scrollReveal = new RevealOnScroll();
+
+
 
 if (module.hot) {
     module.hot.accept();
 }
-
-let menu = new MobileMenu();
