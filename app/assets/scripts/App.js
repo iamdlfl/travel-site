6efaba12df707accbef550d6d0d68d490dbb7f58
David Lynch <davidjlynch1017@gmail.com>
2020-10-10 08:36:19 -0500

    finished reveal on scroll template and implemented for testimonials and feature items

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,8 +3,8 @@ import MobileMenu from './modules/MobileMenu.js'
 import RevealOnScroll from'./modules/RevealOnScroll.js'
 
 let menu = new MobileMenu();
-let scrollReveal = new RevealOnScroll();
-
+new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
+new RevealOnScroll(document.querySelectorAll('.testimonial',), 60);
 
 
 if (module.hot) {
--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,29 +1,55 @@
+import throttle from 'lodash/throttle'
+import debounce from 'lodash/debounce'
+
 class RevealOnScroll {
-    constructor() {
-        this.itemsToReveal = document.querySelectorAll(".feature-item");
+    constructor(docSelector, threshPerc) {
+        this.thresholdPercent = threshPerc
+        this.itemsToReveal = docSelector;
         this.hideInitially();
+        this.scrollThrottle = throttle(this.calcCaller, 200).bind(this);
         this.events();
+        this.windowHeight = window.innerHeight;
     }
     
     calculateIfScrolledTo(el) {
-        let scrollPercent = (el.getBoundingClientRect().y / window.innerHeight) * 100;
-        if (scrollPercent < 75) {
-            el.classList.add('reveal-item--is-visible');
-
+        if (window.scrollY + this.windowHeight > el.offsetTop) {
+            console.log("Calc function ran");
+            let scrollPercent = (el.getBoundingClientRect().y / this.windowHeight) * 100;
+            if (scrollPercent < this.thresholdPercent) {
+                el.classList.add("reveal-item--is-visible");
+                el.isRevealed = true;
+                if (el.isLastItem) {
+                    window.removeEventListener("scroll", this.scrollThrottle);
+                }
+    
+            }
         }
     }
 
-    events() {
-        window.addEventListener("scroll", () => {
-            this.itemsToReveal.forEach(el => {
+    calcCaller() {
+        this.itemsToReveal.forEach(el => {
+            console.log("calc caller ran");
+            if (!el.isRevealed) {
                 this.calculateIfScrolledTo(el);
-            })
+            }
         })
+    }
+
+    events() {
+        window.addEventListener("scroll", this.scrollThrottle);
+        window.addEventListener("resize", debounce(() => {
+            console.log("resizing");
+            this.windowHeight = window.innerHeight;
+        }, 333));
     }
 
 
     hideInitially() {
-        this.itemsToReveal.forEach(el => el.classList.add("reveal-item"));
+        this.itemsToReveal.forEach(el => {
+            el.classList.add("reveal-item");
+            el.isRevealed = false;
+        });
+        this.itemsToReveal[this.itemsToReveal.length -1].isLastItem = true;
     }
 }
 
